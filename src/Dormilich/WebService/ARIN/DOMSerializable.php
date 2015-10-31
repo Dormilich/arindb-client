b0ba50a95ac68824fd46e6e43f1cd9f64ccc2e0f
Bertold von Dormilich <Dormilich@netscape.net>
2015-10-31 10:19:53 +0100

    added interface docs.

--- src/Dormilich/WebService/ARIN/DOMSerializable.php
+++ src/Dormilich/WebService/ARIN/DOMSerializable.php
@@ -2,11 +2,32 @@
 
 namespace Dormilich\WebService\ARIN;
 
+/**
+ * This interface shall guarantee that an element can be transformed into an 
+ * XML element.
+ */
 interface DOMSerializable
 {
+	/**
+	 * The tag name of the base XML element.
+	 * 
+	 * @return string
+	 */
 	public function getName();
 
+	/**
+	 * Determines if an element is empty or valid. The XML serialiser may 
+	 * choose to omit empty elements in the serialisation process.
+	 * 
+	 * @return boolean TRUE if the element scontains (sufficient) data.
+	 */
 	public function isDefined();
 
+	/**
+	 * Transform the element into its XML representation.
+	 * 
+	 * @param DOMDocument $doc 
+	 * @return DOMElement
+	 */
 	public function toDOM(\DOMDocument $doc);
 }
--- src/Dormilich/WebService/ARIN/Elements/ElementInterface.php
+++ src/Dormilich/WebService/ARIN/Elements/ElementInterface.php
@@ -4,11 +4,38 @@ namespace Dormilich\WebService\ARIN\Elements;
 
 use Dormilich\WebService\ARIN\DOMSerializable;
 
+/**
+ * An Element refers to either a single XML element having a text value 
+ * or a collection of other elements.
+ */
 interface ElementInterface extends DOMSerializable
 {
+	/**
+	 * Get the data of the element. This may be a string or an array of 
+	 * strings.
+	 * 
+	 * @return string|array(string) 
+	 */
 	public function getValue();
 
+	/**
+	 * Set the value of the element. If the element is an array type, delete 
+	 * previously set values. If the element is a Payload then the behaviour 
+	 * is implementation dependent and may throw an exception.
+	 * 
+	 * @param mixed $value Element value.
+	 * @return void|self
+	 */
 	public function setValue($value);
 
+	/**
+	 * Add a value to the element. If the element is an array type, append the 
+	 * value to the existing data. If the element is a Payload or single-valued 
+	 * element then it should  conform to the behaviour of 
+	 * ElementInterface::setValue(). 
+	 * 
+	 * @param mixed $value Element value.
+	 * @return void|self
+	 */
 	public function addValue($value);
 }
