e1f44ada581bf0cde7a601a28eacef5212c143af
Dr. Alexander Stranzky <astranzky@velia.net>
2015-11-05 18:44:19 +0100

    added delegation key payload tests.

--- src/Dormilich/WebService/ARIN/Payloads/DelegationKey.php
+++ src/Dormilich/WebService/ARIN/Payloads/DelegationKey.php
@@ -26,6 +26,7 @@ class DelegationKey extends Payload
 	{
 		// no explanation is given, which number represents which algorithm
 		$this->create(new Selection('algorithm', [5, 7, 8]));
+		// a hash value
 		$this->create(new Element('digest'));
 		// guess: validity duration since submission in seconds
 		$this->create(new Integer('ttl', 1));
