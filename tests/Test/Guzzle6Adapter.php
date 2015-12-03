a08d8207962cfbd7b22529d3bdeef71cf00053d3
Dr. Alexander Stranzky <astranzky@velia.net>
2015-12-03 16:27:47 +0100

    added cURL client for live testing.

---
+++ tests/Test/Guzzle6Adapter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Test;
+
+use Dormilich\WebService\Adapter\ClientAdapter;
+use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
+
+class Guzzle6Adapter implements ClientAdapter
+{
+    protected $client;
+
+    protected $baseUri;
+
+    /**
+     * Create instance.
+     * 
+     * @param array $options Guzzle configuration options.
+     * @return self
+     */
+    public function __construct(array $options)
+    {
+        $this->client = new Client($options);
+    }
+
+    /**
+     * Set the Guzzle base URI.
+     * 
+     * @param string $uri Base URI to use.
+     * @return void
+     */
+    public function setBaseUri($uri)
+    {
+        $this->baseUri = $uri;
+    }
+
+    /**
+     * Send a request to the targeted API URI and return the JSON parsed response body.
+     * 
+     * @param string $method HTTP method.
+     * @param string $path Request path.
+     * @param string $body Request body.
+     * @return array JSON parsed response body.
+     */
+    public function request($method, $path, $body = NULL)
+    {
+        $options = ['base_uri' => $this->baseUri];
+
+        if (is_string($body)) {
+            $options['body'] = $body;
+        }
+
+        return $this->client->request($method, $path, $options)->getBody();
+    }
+}
